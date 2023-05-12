4d99c929adfc9bcb643ebfd34f4fdd237beb5d46
SkylerA <skylerarabak@gmail.com>
2023-05-12 07:54:53 -0700

    add missed css changes for multi range slider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+// TODO remove this once this project uses App folder style
+import "@/components/multiRangeSlider/multirangeSlider.css";
+
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
