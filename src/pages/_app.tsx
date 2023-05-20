0da85a425b98eaf478fd3f69738edff9d346b026
SkylerA <skylerarabak@gmail.com>
2023-05-20 10:17:41 -0700

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import "@/styles/globals.css";
 // TODO remove this once this project uses App folder style
 import "@/components/multiRangeSlider/multirangeSlider.css";
+import { Analytics } from "@vercel/analytics/react";
 
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Analytics />
+    </>
+  );
 }
