7ee7f16a1daf0e023c6f040d9d34c2a5ab516a9c
SkylerA <skylerarabak@gmail.com>
2023-06-27 10:28:38 -0700

    Adding className handling to Button overload

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 // Local overload of the MUI button to set some default values for this site
 
 import MuiButton, { ButtonProps } from "@mui/material/Button";
+import classnames from "classnames";
 import React from "react";
 
 interface Props extends ButtonProps {}
@@ -9,7 +10,7 @@ const Button = (props: Props) => {
   return (
     <MuiButton
       sx={{ textTransform: "none" }}
-      className="gradient-bg"
+      className={classnames("gradient-bg", props.className)}
       variant="contained"
       onClick={props.onClick}
       style={props.style}
