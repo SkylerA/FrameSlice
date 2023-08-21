39b2ea0a1b66f8d57ac154770bd8ac0fd2b75619
SkylerA <skylerarabak@gmail.com>
2023-08-21 10:07:18 -0700

    Remove unnecessary semicolon

--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -15,7 +15,7 @@ const baseStyle = {
   outline: "none",
   cursor: "pointer",
   height: "10rem",
-  width: "calc(var(--card-min-w) - (2*var(--border-width)));",
+  width: "calc(var(--card-min-w) - (2*var(--border-width)))",
 };
 
 type Props = {
